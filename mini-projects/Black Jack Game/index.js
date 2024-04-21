b855368a71f8354b53868a1e51e5349482df8df2
mahnoorjanjua505 <mano.flora11@gmail.com>
2024-04-21 03:54:14 -0700

    blackjack game added into mini-project folder

---
+++ mini-projects/Black Jack Game/index.js
@@ -0,0 +1,33 @@
+
+// let firstCard = 1;
+// let secondCard = 10;
+// let sum = firstCard + secondCard ;
+// learning if/else conditons on playing cards 
+// if (sum < 21){
+//     console.log("Do you want to draw a new card? " );
+// }
+// else if (sum === 21){
+//     console.log("Wohoo! You've got a Blackjack!");
+// }
+// else{
+//     console.log("oops! You are out of the Game ");
+// }
+
+// - first condition we're saying that ' if our sum is smaller than 21 then run tht following statement;
+// - 2nd condition we're saying that ' if our sum is strickly equals to 21 then run tht following statement;
+// - 3rd condition, we are writing just else, meaning that if both of the above condition run tht else statement;
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
